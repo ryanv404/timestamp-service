ffd47400f733212ec50ea0de128e5526fde2ccba
Ryan <95035038+ryanv404@users.noreply.github.com>
2021-12-02 15:15:36 -0600

    updated to pass FCC tests

--- routes/router.js
+++ routes/router.js
@@ -3,18 +3,25 @@ const router = express.Router();
 
 router.get("/:id", (req, res) => {
   let {id: timeID} = req.params;
-  timeID = timeID.includes("-") ? timeID : Number(timeID);
-  const date = new Date(timeID);
+  let dateID = Number(timeID);
+  dateID = Number.isNaN(dateID) ? timeID : dateID;
+  const date = new Date(dateID);
 
-  if (!date) {
-    return res.status(404).json({
-      msg: "Please provide a valid timestamp or date string in the URL.",
-    });
+  if (date == "Invalid Date") {
+    return res.status(404).json({error: "Invalid Date"});
   }
+  res.status(200).json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
+});
+
+router.get("/", (req, res) => {
+  const date = new Date();
 
   res.status(200).json({
-    unix: `${Math.floor(date.getTime() / 1000)}`,
-    utc: `${date.toUTCString()}`,
+    unix: date.getTime(),
+    utc: date.toUTCString(),
   });
 });
 
